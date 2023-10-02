2f966c93dd1e72f73e57a262fab0ab7b50602671
Aiden <arpeacoc@ualberta.ca>
2023-10-02 11:46:11 -0600

    comments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from functools import partial
 
 
-# TODO Fix King not being able to defend peices, check is somewhat broken, its because of dummy/manualmove
+# TODO 
 # running the legal_list calculation once per turn and saving it as a global variable
 # underpromotion + graphic, graphic for game end, list of all legal moves in chess notation
 # to be able to see 2 cpus play eachother, it may be that i split up the class storing the board and the class drawing the board in tkinter
@@ -35,15 +35,20 @@ class Board:
         self.fiftymove = 0
         # array keeping track of how many times we reach each position in a game
         self.freemove = [0]
+        #3fold repetition
         self.threefold = False
         self.boardstatehistory = [np.zeros((8, 8), dtype=int)]
+        #TODO implement move history in chess notation
         self.stringtoarr = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
         self.arrtostring = {v: k for k, v in self.stringtoarr.items()}
+        #insufficient material bools
         self.whitebishop = True
         self.blackbishop = True
         self.whiteknight = True
         self.blacknight = True
+        #click on a peice, show the squares it can move
         self.possiblemoves = []
+        #fen string reading for loop
         col = 0
         row = 0
         spaceswitch = 0
@@ -119,14 +124,18 @@ class Board:
                         self.turn = int(i)
                     if whiteturn:
                         self.turn = int(i) + 1
+        #initial position setup (3fold)
         for i in range(8):
             for j in range(8):
                 self.boardstatehistory[0][i, j] = self.state[i, j]
+        #initializing window, and 64 buttons according to the boardstate
         self.win = tk.Tk()
+        #contians all images of peices, "99" is a transparent image
         self.Boarddict = {}
         self.Boarddict["99"] = tk.PhotoImage(file=r"0.png")
         for i in range(8):
             for j in range(8):
+                # square colours
                 if (i + j) % 2 == 0:
                     colour = "#FFBEB0"
                 else:
@@ -136,12 +145,21 @@ class Board:
                 self.win.B = tk.Button(bg=colour, activebackground="lawn green", image=self.Boarddict[strinky],
                                        height=60, width=60, command=partial(self.move, (i, j)))
                 self.win.B.grid(row=i, column=j)
+                # buttonray stays constant, we pass button values to move 
+                # buttonrayclone swaps buttons when moves happen so we can move peices outside of the move method
                 self.buttonray[i, j] = self.win.B
                 self.buttonrayclone[i, j] = self.win.B
 
     def mainloop(self):
         self.win.mainloop()
-
+    # input = square, recurse case (bool), output = all legal moves the peice on the square specified can make
+    # given boardstate. Identity represents the closest legal move every piece has
+    # ie, the bishop in open space has 4 moves closest to it, and relative to the square the bishop is on,
+    # they are (1, 1), (1, -1), (-1, 1), (-1, -1). We then iterate through these base moves to check if they are actually legal
+    # and if they are, we check the next value (2, 2) and so on
+    # bool knight takes care of knight, king, and pawn as their moves do not act in this way, instead, identity represents 
+    # all their legal moves as they are limited. We do some checking for moves like castling, capturing diagonally with a pawn,
+    # and en passant and append the corresponding identity value
     def legal_list(self, row, col, norecurse):
         knight = False
         moves = []
@@ -185,6 +203,7 @@ class Board:
                     identity = []
             if self.state[row - 1, col - 1] != 0 or self.enpassant[row - 1, col - 1] == 2:
                 identity.append((-1, -1))
+            # ensuring our index does not go out of bounds
             if col + 1 < 8 and (self.state[row - 1, col + 1] != 0 or self.enpassant[row - 1, col + 1] == 2):
                 identity.append((-1, 1))
             knight = True
@@ -207,19 +226,26 @@ class Board:
             knight = True
         else:
             identity = []
+        # here we iterate with the base identity of the piece. Norecurse defines whether we should check for a 
+        # legal move or a pseudolegal move. A pseudolegal move is one that by the rules of moving the piece, it can be made
+        # but is not legal as it leaves the king in check. the pseudolegal version is used in the in_check method
         if norecurse:
             for i in range(len(identity)):
                 n = identity[i][0]
                 m = identity[i][1]
                 check = True
                 while check:
+                    # edge of board
                     if row + n > 7 or col + m > 7 or row + n < 0 or col + m < 0:
                         check = False
+                    # empty space
                     elif self.state[row + n, col + m] == 0:
                         moves.append((row + n, col + m))
+                    # enemy piece
                     elif (self.state[row + n, col + m] ^ self.state[row, col]) >> 3 == 1:
                         moves.append((row + n, col + m))
                         check = False
+                    # Your piece
                     else:
                         check = False
                     n += identity[i][0]
@@ -252,7 +278,10 @@ class Board:
                     if knight:
                         check = False
         return moves
-
+    # input = boardstate, square the white king, and black king occupy, square you want to check
+    # output = bool, True or false
+    # when called in_check(), will check if the king whos turn it currently is is in check
+    # when called with a specific square, it will check if a piece of the enemy attacks that square
     def in_check(self, square=(9, 9)):
         if square == (9, 9):
             if self.turn % 2 == 0:
@@ -270,6 +299,8 @@ class Board:
 
         for i in range(8):
             for j in range(8):
+                # check whos turn it is, iterate through all the pseudolegal moves the opponents peices can make. 
+                # if they can capture your king, you are in check.
                 if (self.state[i, j] ^ self.state[colorsquare[0], colorsquare[1]]) >> 3 == 1 and self.state[i, j] != 0:
                     legalmoves = self.legal_list(i, j, True)
                     if (kingsquare[0], kingsquare[1]) in legalmoves:
@@ -280,18 +311,26 @@ class Board:
         else:
             self.cflag = 0
             return False
-
+    # input = boardstate, many variables to handle draw conditions, button object you clicked on
+    # return only when game has ended. This method is long and so I split it up into sections with comments
+    # this function works by recieving 2 button objects that were clicked, and seeing if it makes sense as move
+    # it then checks if the move is legal, and if it is, it will make the move on the board
+    # we implement making a move by swapping the button positions in the grid and changing the image and colour of the buttons
+    # moved if need be
     def move(self, c):
         bname = self.buttonray[c]
         self.movestore.append(bname)
+        # if you click on a blank square first, disregard
         if len(self.movestore) == 1 and self.state[self.movestore[0].grid_info()["row"], self.movestore[0].grid_info()["column"]] == 0:
             self.movestore.clear()
+        # yellow colour for potential moves after selecting a peice
         if len(self.movestore) == 1 and self.turn%2 == 0:
             info = bname.grid_info()
             self.possiblemoves = self.legal_list(info["row"], info["column"], False)
             for i in range(len(self.possiblemoves)):
                 self.buttonrayclone[self.possiblemoves[i]].config(bg="#FFFF00")
         if len(self.movestore) == 2:
+            # setting back to normal colour
             if self.turn%2 == 0:
                 for i in range(len(self.possiblemoves)):
                     if (self.possiblemoves[i][0]+self.possiblemoves[i][1]) % 2 == 0:
@@ -299,24 +338,30 @@ class Board:
                     else:
                         self.buttonrayclone[self.possiblemoves[i]].config(bg="#A64A36")
             self.possiblemoves = []
+            # retrieving information about the buttons
             info1 = self.movestore[0].grid_info()
             info2 = self.movestore[1].grid_info()
             piece = (info1["row"], info1["column"])
             square = (info2["row"], info2["column"])
             moves = self.legal_list(piece[0], piece[1], False)
+            # if move is legal
             if square in moves and (
                     self.state[piece] >= 9 and self.turn % 2 == 1 or self.state[piece] < 9 and self.turn % 2 == 0):
+                # setting square it moved from to blank correctly coloured square
                 if (info1["row"] + info1["column"]) % 2 == 0:
                     self.movestore[1].config(bg="#FFBEB0", image=self.Boarddict["99"])
                 else:
                     self.movestore[1].config(bg="#A64A36", image=self.Boarddict["99"])
-
                 if (info2["row"] + info2["column"]) % 2 == 0:
                     self.movestore[0].config(bg="#FFBEB0")
                 else:
                     self.movestore[0].config(bg="#A64A36")
+                # swapping button positions on grid (not in buttonray)
                 self.movestore[0].grid(row=info2["row"], column=info2["column"])
                 self.movestore[1].grid(row=info1["row"], column=info1["column"])
+                
+                # en passant handling, if you are making a move with the properties of en passant
+                # then change the square behind the pawn to empty
                 if self.state[piece] == 0b0001 and self.state[square] == 0 and (
                         abs(piece[0] - square[0]), abs(piece[1] - square[1])) == (1, 1):
                     self.buttonrayclone[square[0] + 1, square[1]].config(image=self.Boarddict["99"])
@@ -325,6 +370,8 @@ class Board:
                         abs(piece[0] - square[0]), abs(piece[1] - square[1])) == (1, 1):
                     self.buttonrayclone[square[0] - 1, square[1]].config(image=self.Boarddict["99"])
                     self.state[square[0] - 1, square[1]] = 0
+
+                # 50 move rule, if no move has been made, and no captures, increment by 1
                 if self.state[piece] & 0b0111 != 0b0001 and self.state[square] == 0:
                     self.fiftymove += 1
                     if self.fiftymove == 100:
@@ -332,12 +379,17 @@ class Board:
                         return
                 else:
                     self.fiftymove = 0
+                
+                #swap the pieces inside our board representation
                 self.state[square] = self.state[piece]
                 self.state[piece] = 0
                 self.turn += 1
+
                 if self.insufficient():
                     print("Insufficient material, Draw!")
                     return
+                
+                #3fold handling, each element in freemove represents a unique boardstate
                 for i in range(len(self.boardstatehistory)):
                     comparison = self.state == self.boardstatehistory[i]
                     if comparison.all():
@@ -353,6 +405,8 @@ class Board:
                         const[i, j] = self.state[i, j]
                 self.boardstatehistory.append(const)
                 self.freemove.append(0)
+
+                #king moves for castling purposes
                 if self.state[square] == 0b0110:
                     self.kingmove[1] = True
                 if self.state[square] == 0b1110:
@@ -360,6 +414,8 @@ class Board:
                 self.whiteking = np.asarray(np.where(self.state == 6)).T[0].tolist()
                 self.blacking = np.asarray(np.where(self.state == 14)).T[0].tolist()
                 self.castle(piece, square)
+
+                # en passant checking, after a pawn moves 2 squares forward, it is vulnerable to en passant, but only on that turn
                 for i in range(8):
                     self.enpassant[2, i] = 0
                     self.enpassant[5, i] = 0
@@ -368,22 +424,26 @@ class Board:
                         self.enpassant[piece[0] + 1, piece[1]] = 2
                     if piece[0] == 6 and self.state[square] == 0b0001:
                         self.enpassant[piece[0] - 1, piece[1]] = 1
+                    
                 x = self.checkmate()
                 if x is not None:
                     print(x)
                     return
-
+                
+                # swapping buttonrayclone for button positions, making moves not through interacting with board
                 store = self.buttonrayclone[piece]
                 self.buttonrayclone[piece] = self.buttonrayclone[square]
                 self.buttonrayclone[square] = store
+                # promotion
                 if self.state[square] & 0b0111 == 0b0001 and (square[0] == 0 or square[0] == 7):
                     self.promote(square)
-                print(self.whiteking, self.blacking)
+            # bot call, checks if its the bots turn, and the game has not ended
             if self.turn % 2 == 1 and self.fiftymove != 100 and self.threefold == False and not self.insufficient():
                 self.movestore.clear()
                 self.automove()
             self.movestore.clear()
 
+    # handles all logic for castling, will ensure that kings can only castle when the specific conditions are met
     def castle(self, piece, square):
         if self.state[piece] == 0b0010 and piece == (7, 0):
             self.rook[2] = True
@@ -481,7 +541,8 @@ class Board:
             self.buttonrayclone[0, 5] = store
             self.state[0, 5] = 0b1010
             self.state[0, 7] = 0b0000
-
+    
+    # We check every turn if the opponent has any legal moves they can make, if they do, return nothing, else return who wins/stalemate
     def checkmate(self):
         if self.turn % 2 == 0:
             n = 0
@@ -502,6 +563,7 @@ class Board:
         elif not self.threefold and not self.fiftymove == 100 and not self.insufficient():
             return "Stalemate"
 
+    # button changing for promotion, and board representation change
     def promote(self, square):
         if square[0] == 0:
             self.state[0, square[1]] = 0b0101
@@ -510,6 +572,7 @@ class Board:
             self.state[7, square[1]] = 0b1101
             self.buttonrayclone[7, square[1]].config(image=self.Boarddict["03"])
 
+    # checks the board for insufficient material 
     def insufficient(self):
         for i in range(8):
             for j in range(8):
@@ -538,6 +601,7 @@ class Board:
         else:
             return True
 
+    # special function used for computer opponents, similar to manualmove/undomove but in a single function
     def dummymove(self, piece, square, undo):
         if piece == square:
             return
@@ -557,7 +621,9 @@ class Board:
             self.state[square] = self.takenpiece
             self.whiteking = np.asarray(np.where(self.state == 6)).T[0].tolist()
             self.blacking = np.asarray(np.where(self.state == 14)).T[0].tolist()
-
+    
+    # will make move on board representation, partner method with undomanual, as it does not affect gui.
+    # used for making a move and checking if you are in check, if you are, you cant make the move
     def manualmove(self, piece, square):
         x = self.state[square]
         self.state[square] = self.state[piece]
@@ -572,6 +638,9 @@ class Board:
         self.whiteking = np.asarray(np.where(self.state == 6)).T[0].tolist()
         self.blacking = np.asarray(np.where(self.state == 14)).T[0].tolist()
 
+    #TODO move to different file 
+    # Computer opponent, plays black. Follows logic of if checkmate then do that
+    # if not then check, if not then capture, if not then make a random move
     def automove(self):
         moves = []
         for i in range(8):
